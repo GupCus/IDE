ba2ffa84186f689e92db69354e76b8d260a37689
Agus <abarrosobol@frro.utn.edu.ar>
2025-09-14 17:55:31 -0300

    Lab U5 terminado

---
+++ Unidad5- Apps Web/Lab01/Lab01/Components/Clases/Alumno.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace Lab01.Components.Clases
+{
+    public class Alumno
+    {
+        public int? Id { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
+        public required string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public required string Nombre { get; set; }
+
+        [Range(1, 99999, ErrorMessage = "El legajo tiene que ser mayor que 0 y menor a 99999")]
+        public required int Legajo { get; set; }
+        [Required(ErrorMessage = "La direccion es obligatoria.")]
+        public required string Direccion { get; set; }
+
+        public Alumno(int id, string apellido, string nombre, int legajo, string direccion)
+        {
+            Id = id;
+            Apellido = apellido;
+            Nombre = nombre;
+            Legajo = legajo;
+            Direccion = direccion;
+        }
+        public Alumno() { }
+
+        public new string ToString()
+        {
+            return $"Alumno: {Apellido}, {Nombre}  || Id Interno: {Id}\nLegajo: {Legajo}\nDireccion: {Direccion}";
+        }
+    }
+}
---
+++ Unidad5- Apps Web/Lab01/Lab01/Components/Clases/UniversidadContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab01.Components.Clases
+{
+    internal class UniversidadContext: DbContext
+    {
+        public DbSet<Alumno> Alumnos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS;
+                                        Initial Catalog=Universidad;
+                                        Integrated Security=true;
+                                        TrustServerCertificate=True");
+
+            optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+        }
+
+        public UniversidadContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        /*
+            TIP: instanciar la clase UniversidadContext anteponiendo la palabra clave using a la
+            declaración de la variable; de esta manera, nos aseguraremos de cerrar la conexión con la base
+            de datos y liberar recursos cuando finalice su utilización.
+        */
+    }
+}
+
